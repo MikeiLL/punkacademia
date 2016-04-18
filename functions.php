a8da67f3c3c5d1b273a65beab3911d35bf106c8d
Mike iLL Kilmer <mike@mzoo.org>
2016-04-18 16:46:11 -0500

    Update textdomain.

--- functions.php
+++ functions.php
@@ -15,12 +15,13 @@ $sage_includes = [
   'lib/setup.php',     // Theme setup
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
-  'lib/customizer.php' // Theme customizer
+  'lib/customizer.php', // Theme customizer
+  'lib/wp_bootstrap_navwalker.php' // Theme customizer
 ];
 
 foreach ($sage_includes as $file) {
   if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+    trigger_error(sprintf(__('Error locating %s for inclusion', 'punkacademia'), $file), E_USER_ERROR);
   }
 
   require_once $filepath;
