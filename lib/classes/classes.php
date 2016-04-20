b82eb70132a20964093bd1b98dccb687c1212748
Mike iLL Kilmer <mike@mzoo.org>
2016-04-20 02:40:25 -0500

    Add taxonomy for classes.

--- lib/classes/classes.php
+++ lib/classes/classes.php
@@ -61,7 +61,7 @@ use DateTimeZone;
 			),
 			'has_archive'   => true,
 			'hierarchical'   => true,
-			'taxonomies' => array( 'category' ),
+			'taxonomies' => array( 'topics' ),
 			'rewrite' => array(
 				'slug' => 'classes',
 			)
@@ -70,9 +70,41 @@ use DateTimeZone;
 	}
 	add_action( 'init', __NAMESPACE__ . '\\punkacademia_classes_add_custom_post_type' );
 
+//create a custom taxonomy name it topics for your posts
+function create_topics_hierarchical_taxonomy() {
 
+// Add new taxonomy, make it hierarchical like categories
+//first do the translations part for GUI
 
+  $labels = array(
+    'name' => _x( 'Topics', 'taxonomy general name' ),
+    'singular_name' => _x( 'Topic', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Topics' ),
+    'all_items' => __( 'All Topics' ),
+    'parent_item' => __( 'Parent Topic' ),
+    'parent_item_colon' => __( 'Parent Topic:' ),
+    'edit_item' => __( 'Edit Topic' ),
+    'update_item' => __( 'Update Topic' ),
+    'add_new_item' => __( 'Add New Topic' ),
+    'new_item_name' => __( 'New Topic Name' ),
+    'menu_name' => __( 'Topics' ),
+  );
 
+// Now register the taxonomy
+
+  register_taxonomy('topics',array('post'), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'topic' ),
+  ));
+
+}
+
+//hook into the init action and call create_book_taxonomies when it fires
+add_action( 'init', __NAMESPACE__ . '\\create_topics_hierarchical_taxonomy', 0 );
 
 	/**
 	 * Save classes data to revisions
